76dbecee2825cadf2e261ca4dbbf2a541e025828
david murage <110826121+davidmurage@users.noreply.github.com>
2023-10-23 08:23:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION["user"])) {
 </head>
 <body>
     <div class="container">
-        <h1>Welcome to Dashboard</h1>
+        <h1>Welcome to user Dashboard</h1>
         <a href="logout.php" class="btn btn-warning">Logout</a>
     </div>
 </body>
